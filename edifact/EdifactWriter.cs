e53a9b0c968b5205c74ddd0660bc3b1938e5b1d4
Martin Ahlström <martin.ahlstrom@outlook.com>
2015-05-03 19:38:35 +0200

    Add a writer

---
+++ edifact/EdifactWriter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Edifact
+{
+	public static class EdifactWriter
+	{
+		public static void WriteFile(ISettings settings, List<Segment> segments, FileInfo fi)
+		{
+			using (var f = new StreamWriter(fi.FullName, false, Encoding.UTF8))
+			{
+				f.Write(WriteData(settings, segments));
+			}
+		}
+		public static string WriteData(ISettings settings, IEnumerable<Segment> segments)
+		{
+			var result = new StringBuilder(512);
+
+			foreach (var sgmnt in segments)
+			{
+				if (result.Length > 0)
+					result.AppendLine();
+				result.Append(sgmnt.ToString(settings));
+				result.Append(settings.SegmentTerminator);				
+			}
+
+			return result.ToString();
+		}
+	}
+}
