8b88a38476a9d5aa3cbee4134bc96db9c356102a
Martin Ahlström <martin.ahlstrom@outlook.com>
2015-05-04 15:14:36 +0200

    readding

---
+++ edifact/SpecifiedSegments/LinSegment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edifact.SpecifiedSegments
+{
+	public class LinSegment : Segment
+	{
+		public LinSegment()
+			: base()
+		{
+		}
+		public LinSegment(string segmentValue, char elementSeparator, char componentSeparator) :
+			base(segmentValue, elementSeparator, componentSeparator)
+		{
+		}
+
+		public int LineNumber
+		{
+			get
+			{
+				int v;
+				if (int.TryParse(base[1, 0], out v))
+					return v;
+				return 0;
+			}
+			set { base[1, 0] = value.ToString(); }
+		}
+		public string Identifier { get { return base[3, 0]; } set { base[3, 0] = value; } }
+		public string IdentifierType { get { return base[3, 1]; } set { base[3, 1] = value; } }
+	}
+}
---
+++ edifact/SpecifiedSegments/NadSegment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edifact.SpecifiedSegments
+{
+	public class NadSegment : Segment
+	{
+		public NadSegment()
+			: base()
+		{ }
+		public NadSegment(string segmentValue, char elementSeparator, char componentSeparator) :
+			base(segmentValue, elementSeparator, componentSeparator)
+		{
+
+		}
+
+		public string NadType { get { return base[1, 0]; } set { base[1, 0] = value; } }
+		public string Identification { get { return base[2, 0]; } set { base[2, 0] = value; } }
+		public string ResponsibleAgencyCode { get { return base[2, 2]; } set { base[2, 2] = value; } }
+		public string Name1 { get { return base[4, 0]; } set { base[4, 0] = value; } }
+		public string Name2 { get { return base[4, 1]; } set { base[4, 1] = value; } }
+		public string Address1 { get { return base[5, 0]; } set { base[5, 0] = value; } }
+		public string Address2 { get { return base[5, 1]; } set { base[5, 1] = value; } }
+		public string Address3 { get { return base[5, 2]; } set { base[5, 2] = value; } }
+		public string City { get { return base[6, 0]; } set { base[6, 0] = value; } }
+		public string PostalCode { get { return base[8, 0]; } set { base[8, 0] = value; } }
+		public string CountryCode { get { return base[9, 0]; } set { base[9, 0] = value; } }
+	}
+}
